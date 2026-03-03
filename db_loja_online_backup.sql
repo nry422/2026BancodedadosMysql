ec2b626803d3b7bf103ba85c0bcc07f8446027f2
nry422 <henrymuller@gmail.com>
2026-03-03 16:52:21 -0300

    atualizacao
    atualizacao

--- db_loja_online_backup.sql
+++ db_loja_online_backup.sql
@@ -4,15 +4,24 @@
 -- ------------------------------------------------------
 -- Server version	10.4.32-MariaDB
 
---CREATE DATABASE lojaonline;
---USE lojaonline;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `endereco`
 --
 
 DROP TABLE IF EXISTS `endereco`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `endereco` (
   `id` int(11) DEFAULT NULL,
   `bairro` varchar(45) DEFAULT NULL,
@@ -21,14 +30,15 @@ CREATE TABLE `endereco` (
   `estado` varchar(45) DEFAULT NULL,
   `complementos` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `endereco`
 --
 
 LOCK TABLES `endereco` WRITE;
-
+/*!40000 ALTER TABLE `endereco` DISABLE KEYS */;
+/*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -36,7 +46,8 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `estoque`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `estoque` (
   `codigo` int(11) DEFAULT NULL,
   `preco` decimal(8,2) DEFAULT NULL,
@@ -45,14 +56,15 @@ CREATE TABLE `estoque` (
   `quantidade` int(11) DEFAULT NULL,
   `categoria` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `estoque`
 --
 
 LOCK TABLES `estoque` WRITE;
-
+/*!40000 ALTER TABLE `estoque` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estoque` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -60,19 +72,22 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `itens`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `itens` (
   `id` int(11) DEFAULT NULL,
   `qtde` int(11) DEFAULT NULL,
   `des` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `itens`
 --
 
 LOCK TABLES `itens` WRITE;
-
+/*!40000 ALTER TABLE `itens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `itens` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -80,21 +95,23 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `pedido`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pedido` (
   `id` int(11) DEFAULT NULL,
   `nfiscal` varchar(100) DEFAULT NULL,
   `valortotal` decimal(10,2) DEFAULT NULL,
   `datahora` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `pedido`
 --
 
 LOCK TABLES `pedido` WRITE;
-
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -102,7 +119,8 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `usuario`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
   `codigo` int(11) DEFAULT NULL,
   `nome` varchar(100) DEFAULT NULL,
@@ -113,17 +131,24 @@ CREATE TABLE `usuario` (
   `perfil` int(11) DEFAULT NULL,
   `senha` varchar(25) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usuario`
 --
 
 LOCK TABLES `usuario` WRITE;
-
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2026-02-24 17:45:31
