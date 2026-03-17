8e7c2dcdbe0379216f0f6def6f1eede8d16b7f53
nry422 <henrymuller@gmail.com>
2026-03-17 17:18:23 -0300

    atualizando script

--- script_lojaonline.sql
+++ script_lojaonline.sql
@@ -10,33 +10,43 @@ CREATE TABLE Produto(
 	tipo ENUM('Novo', 'Usado', 'Liquidacao', 'Promocao', 'Outros'),
 	categoria SET('Eletronico', 'Telefonia', 'Informatica', 'Eletrodomesticos', 'Acessorios', 'Outros'),
 	datalancamento DATE,
-	descontousado(10,2),
+	descontousado DECIMAL(10,2)
 	);
 	
 	CREATE TABLE Loja(
 	id INT PRIMARY KEY,
 	nome VARCHAR(200),
 	endereco VARCHAR(255),
-	telefone VARCHAR(45),
-	):
+	telefone VARCHAR(45)
+	);
 	
 	CREATE TABLE Caracteristicas(
 	id INT PRIMARY KEY,
 	nome VARCHAR(200),
-	descricao VARCHAR(255),
+	descricao VARCHAR(255)
 	);
 	
 	CREATE TABLE Estoque(	
-	loja_id INT PRIMARY KEY,
-	produto_id INT PRIMARY KEY,
+	loja_id INT,
+	produto_id INT,
 	FOREIGN KEY(loja_id) REFERENCES Loja (id),
 	FOREIGN KEY (produto_id) REFERENCES Produto (id),
+	PRIMARY KEY (loja_id, produto_id)
 	);
 	
-	CREATE TABLE Produto-Caracteristicas(	
-	produto_id INT PRIMARY KEY,
-	caracteristicas_id INT PRIMARY KEY,
+	CREATE TABLE Produto_Caracteristicas(	
+	produto_id INT,
+	caracteristicas_id INT,
 	FOREIGN KEY(produto_id) REFERENCES Produto (id),
 	FOREIGN KEY (caracteristicas_id) REFERENCES Caracteristicas (id),
+	PRIMARY KEY (produto_id, caracteristicas_id)
+	
 	);
 	
+	--População do Banco de dados (inserir dados no banco de dados)
+	
+INSERT INTO Loja(id, nome, endereco, telefone) VALUES
+(1, 'Nova Loja', 'Rua Domingos da Nova, 333', '(47) 3371-1321');
+
+INSERT INTO Produto(id, nome, descricao, preco, tipo, categoria, datalancamento, descontousado) VALUES
+(1, 'Placa de Video Radeon RX 480 8GB', 'RX 480 Sapphire Dual Cooler', 650.0, 'Novo', 'Informatica', '2016-04-12', 0.0);
