216422003f56b45df2a52dd412a89e4400666531
nry422 <henrymuller@gmail.com>
2026-03-17 17:40:08 -0300

    atualização
    Durante aula

--- script_lojaonline.sql
+++ script_lojaonline.sql
@@ -29,6 +29,7 @@ CREATE TABLE Produto(
 	CREATE TABLE Estoque(	
 	loja_id INT,
 	produto_id INT,
+	quantidade INT,
 	FOREIGN KEY(loja_id) REFERENCES Loja (id),
 	FOREIGN KEY (produto_id) REFERENCES Produto (id),
 	PRIMARY KEY (loja_id, produto_id)
@@ -48,5 +49,34 @@ CREATE TABLE Produto(
 INSERT INTO Loja(id, nome, endereco, telefone) VALUES
 (1, 'Nova Loja', 'Rua Domingos da Nova, 333', '(47) 3371-1321');
 
+INSERT INTO Loja(id, nome, endereco, telefone) VALUES
+(2, 'Segunda Loja', 'Rua Marechal Deodoro, 133', '(47) 3372-7321');
+
 INSERT INTO Produto(id, nome, descricao, preco, tipo, categoria, datalancamento, descontousado) VALUES
 (1, 'Placa de Video Radeon RX 480 8GB', 'RX 480 Sapphire Dual Cooler', 650.0, 'Novo', 'Informatica', '2016-04-12', 0.0);
+
+INSERT INTO Produto(id, nome, descricao, preco, tipo, categoria, datalancamento, descontousado) VALUES
+(2, 'Placa de Video RTX 5090 24GB', 'RTX 5090 Asus', 7650.0, 'Novo', 'Informatica', '2024-11-12', 0.0);
+
+INSERT INTO Produto(id, nome, descricao, preco, tipo, categoria, datalancamento, descontousado) VALUES
+(3, 'Mouse USB', 'Mouse USB Genérico', 15.50, 'Novo', 'Informatica', '2018-04-12', 0.0);
+
+INSERT INTO Produto(id, nome, descricao, preco, tipo, categoria, datalancamento, descontousado) VALUES
+(4, 'Teclado USB', 'Teclado USB Genérico', 9.50, 'Usado', 'Informatica', '2020-04-12', 5.0);
+
+INSERT INTO  Estoque(loja_id, produto_id, quantidade) VALUES
+(1, 1, 50);
+
+INSERT INTO  Estoque(loja_id, produto_id, quantidade) VALUES
+(1, 2, 25);
+
+INSERT INTO  Estoque(loja_id, produto_id, quantidade) VALUES
+(2, 3, 75);
+
+INSERT INTO  Estoque(loja_id, produto_id, quantidade) VALUES
+(2, 4, 150);
+
+
+
+
+
