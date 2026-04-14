0c8e54117d2d7aa93d3fac0fffa35b3f7c331a97
nry422 <henrymuller@gmail.com>
2026-04-14 16:46:13 -0300

    Prova 01

---
+++ Prova01_correcao/questoes_prova.sql
@@ -0,0 +1,88 @@
+--Questoes Prova
+--Questao 3
+
+CREATE DATABASE cinefun;
+
+USE cinefun;
+
+--Questao 4
+
+CREATE TABLE Sala(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	tam_tela_pol DECIMAL(8,2),
+	area_m DECIMAL(8, 2),
+	status ENUM('disponivel', 'limpeza', 'manutenção'),
+	capacidade INT 
+);
+
+CREATE TABLE Filme(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(45) NOT NULL,
+	classificacao VARCHAR(45),
+	duracao INT
+);
+
+CREATE TABLE Sessaoes(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	data DATE,
+	hora TIME,
+	preco DECIMAL(8,2),
+	sala INT,
+	filme INT,
+	FOREIGN KEY (sala) REFERENCES Sala(id),
+	FOREIGN KEY (filme) REFERENCES Filme(id)
+
+);
+
+CREATE TABLE Clientes(
+	cpf VARCHAR(30) PRIMARY KEY,
+	nome_completo VARCHAR(200) NOT NULL,
+	telefone VARCHAR(45)
+);
+
+--Questao 5 comandos DDL
+
+ALTER TABLE Clientes 
+ADD COLUMN data_nascimento DATE
+DEFAULT "2026-04-14"
+
+--DML Questão 6
+
+INSERT INTO Sala(id, tam_tela_pol, area_m, status, capacidade) VALUES
+(NULL, 600, 50.5, "disponivel", 150),
+(NULL, 900, 100, "limpeza", 500);
+
+INSERT INTO Filme VALUES
+(NULL, "Avatar I", "Maiores de 12 anos", 120),
+(NULL, "Crepusculo", "Maiores de 16 anos", 90);
+
+--Questao 7
+INSERT INTO Sessoes VALUES
+(NULL, '2026-04-14', '19:00', 25.80, 1, 1);
+
+--Questao 8
+
+UPDATE Clientes SET telefone = "+55 (47) 98866-8866"
+WHERE cpf = "000.098.098-87";
+
+--Questao 9
+
+DELETE FROM Filme WHERE id = 10;
+
+--Questão 10
+--DCL
+
+CREATE USER 'user_ext'@'localhost' IDENTIFIED BY 'senha0101';
+
+--Questao 11
+
+GRANT SELECT, INSERT ON Cinefun.* TO 'user_ext'@'localhost';
+
+
+
+
+
+
+
+
+
